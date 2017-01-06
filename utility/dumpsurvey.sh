47b1b4176f5c6bd0a8e849611fa3eddd6b920318
DevCoding226 <ilovedevcoding@gmail.com>
2017-01-06 15:21:11 +0200

    Survey sump

--- utility/dumpsurvey.sh
+++ utility/dumpsurvey.sh
@@ -1,1 +1,8 @@
-./manage.py dumpdata --indent 2 survey.question survey.option survey.survey survey.surveyitem
+#!/bin/bash
+
+WORK_DIR="$(dirname "$0")"
+cd $WORK_DIR/..
+
+
+
+./manage.py dumpdata --indent 2 survey.question survey.option survey.survey survey.surveyitem > docs/survey.json
