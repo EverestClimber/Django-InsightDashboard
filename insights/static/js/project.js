032a6c1b2a2571c8bfc2d85dd2edbbdc1502cc27
DevCoding226 <ilovedevcoding@gmail.com>
2017-01-20 16:34:34 +0300

    feat(reports menu): basic submenus

--- insights/static/js/project.js
+++ insights/static/js/project.js
@@ -317,8 +317,18 @@ $(document).ready(function () {
               }
         });
 
+        // Init submenu
 
-
+        $( ".dropdown-submenu" ).click(function(event) {
+            // stop bootstrap.js to hide the parents
+            event.stopPropagation();
+            // hide the open children
+            $( this ).find(".dropdown-submenu").removeClass('open');
+            // add 'open' class to all parents with class 'dropdown-submenu'
+            $( this ).parents(".dropdown-submenu").addClass('open');
+            // this is also open (or was)
+            $( this ).toggleClass('open');
+        });
     });
 
 
