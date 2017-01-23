6e330fbb3f71966046b9f5d47da8985419a5c097
DevCoding226 <ilovedevcoding@gmail.com>
2017-01-23 23:34:51 +0300

    feat(basic report): q2 pie chart

--- insights/static/js/reports-basic.js
+++ insights/static/js/reports-basic.js
@@ -27,6 +27,35 @@
     drawPie(q1PieData, '#q1Pie', '#q1PieLegend')
   }
 
+  function drawQ2Pie() {
+    var q2PieData = {
+      labels: [
+        "No difference",
+        "Treats differently"
+      ],
+      datasets: [
+        {
+          data: [128, 72],
+          backgroundColor: [
+            "#b8103b",
+            "#328ec9"
+          ],
+          hoverBackgroundColor: [
+            "#b8103b",
+            "#328ec9"
+          ]
+        }]
+    };
+
+    var yesNum = q2PieData.datasets[0].data[0];
+    var noNum = q2PieData.datasets[0].data[1];
+
+    $('#q2PieYes').css('color', q2PieData.datasets[0].backgroundColor[0]).html(yesNum);
+    $('#q2PieNo').css('color', q2PieData.datasets[0].backgroundColor[1]).html(noNum);
+
+    drawPie(q2PieData, '#q2Pie', '#q2PieLegend')
+  }
+
   function drawPie(pieData, pieId, legendId) {
     var pieCtx = $(pieId).get(0).getContext("2d");
     var pieLegend = $(legendId);
@@ -45,5 +74,6 @@
 
   $(document).ready(function() {
     drawQ1Pie();
+    drawQ2Pie();
   })
 })();
