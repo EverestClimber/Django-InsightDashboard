339840dc9df01c39a6c26ffe0bfed0beb25ff87c
DevCoding226 <ilovedevcoding@gmail.com>
2017-01-23 20:35:18 +0300

    feat(charts): pie draw function

--- insights/static/js/reports-basic.js
+++ insights/static/js/reports-basic.js
@@ -1,33 +1,43 @@
 (function () {
-  var q1PieData = {
-    labels: [
-      "No abuse",
-      "Substance abuse"
-    ],
-    datasets: [
-      {
-        data: [128, 72],
-        backgroundColor: [
-          "#b8103b",
-          "#328ec9"
-        ],
-        hoverBackgroundColor: [
-          "#b8103b",
-          "#328ec9"
-        ]
-      }]
-  };
-  var q1PieCtx = $("#q1Pie").get(0).getContext("2d");
-  var q1PieLegend = $('#q1PieLegend');
-  var q1Pie = new Chart(q1PieCtx,{
-    type: 'pie',
-    data: q1PieData,
-    options: {
-      legend: false,
-      tooltips: {
-        enabled: false
+  function drawQ1Pie() {
+    var q1PieData = {
+      labels: [
+        "No abuse",
+        "Substance abuse"
+      ],
+      datasets: [
+        {
+          data: [128, 72],
+          backgroundColor: [
+            "#b8103b",
+            "#328ec9"
+          ],
+          hoverBackgroundColor: [
+            "#b8103b",
+            "#328ec9"
+          ]
+        }]
+    };
+    drawPie(q1PieData, '#q1Pie', '#q1PieLegend')
+  }
+
+  function drawPie(pieData, pieId, legendId) {
+    var pieCtx = $(pieId).get(0).getContext("2d");
+    var pieLegend = $(legendId);
+    var pie = new Chart(pieCtx,{
+      type: 'pie',
+      data: pieData,
+      options: {
+        legend: false,
+        tooltips: {
+          enabled: false
+        }
       }
-    }
-  });
-  q1PieLegend.html(q1Pie.generateLegend());
+    });
+    pieLegend.html(q1Pie.generateLegend());
+  }
+
+  $(document).ready(function() {
+    drawQ1Pie();
+  })
 })();
