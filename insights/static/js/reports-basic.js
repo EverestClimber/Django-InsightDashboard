3b9a3da07e85a4c59eb63ea1ac39156b141bdf00
DevCoding226 <ilovedevcoding@gmail.com>
2017-01-23 18:12:35 +0300

    feat(basic reports): pie tooltip

--- insights/static/js/reports-basic.js
+++ insights/static/js/reports-basic.js
@@ -29,6 +29,57 @@
     }
   });
 
+  Chart.defaults.global.tooltips.custom = function(tooltip) {
+    // Tooltip Element
+    var tooltipEl = document.getElementById('chartjs-tooltip');
+    // Hide if no tooltip
+    if (tooltip.opacity === 0) {
+      tooltipEl.style.opacity = 0;
+      return;
+    }
+    // Set caret Position
+    tooltipEl.classList.remove('above', 'below', 'no-transform');
+    if (tooltip.yAlign) {
+      tooltipEl.classList.add(tooltip.yAlign);
+    } else {
+      tooltipEl.classList.add('no-transform');
+    }
+    function getBody(bodyItem) {
+      return bodyItem.lines;
+    }
+    // Set Text
+    if (tooltip.body) {
+      var titleLines = tooltip.title || [];
+      var bodyLines = tooltip.body.map(getBody);
+      var innerHtml = '<thead>';
+      titleLines.forEach(function(title) {
+        innerHtml += '<tr><th>' + title + '</th></tr>';
+      });
+      innerHtml += '</thead><tbody>';
+      bodyLines.forEach(function(body, i) {
+        var colors = tooltip.labelColors[i];
+        var style = 'background:' + colors.backgroundColor;
+        style += '; border-color:' + colors.borderColor;
+        style += '; border-width: 2px';
+        var span = '<span class="chartjs-tooltip-key" style="' + style + '"></span>';
+        innerHtml += '<tr><td>' + span + body + '</td></tr>';
+      });
+      innerHtml += '</tbody>';
+      var tableRoot = tooltipEl.querySelector('table');
+      tableRoot.innerHTML = innerHtml;
+    }
+    var position = this._chart.canvas.getBoundingClientRect();
+    var $canvas = $(this._chart.canvas);
+    // Display, position, and set styles for font
+    tooltipEl.style.opacity = 1;
+    tooltipEl.style.left = position.left + tooltip.caretX + 'px';
+    tooltipEl.style.top = $canvas.offset().top + tooltip.caretY + 'px';
+    tooltipEl.style.fontFamily = 'open-sans';
+    tooltipEl.style.fontSize = tooltip.fontSize;
+    tooltipEl.style.fontStyle = tooltip._fontStyle;
+    tooltipEl.style.padding = tooltip.yPadding + 'px ' + tooltip.xPadding + 'px';
+  };
+
   var q1PieData = {
     labels: [
       "No abuse",
@@ -36,7 +87,7 @@
     ],
     datasets: [
       {
-        data: [64, 36],
+        data: [128, 72],
         backgroundColor: [
           "#b8103b",
           "#328ec9"
@@ -54,7 +105,9 @@
     data: q1PieData,
     options: {
       legend: false,
-      showTooltips: true
+      tooltips: {
+        enabled: false
+      }
     }
   });
   q1PieLegend.html(q1Pie.generateLegend());
