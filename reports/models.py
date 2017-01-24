58b029ef06886386f5d39b1c1b109a03b2189825
DevCoding226 <ilovedevcoding@gmail.com>
2017-01-24 17:08:41 +0200

    Charts structure

--- reports/models.py
+++ reports/models.py
@@ -1,8 +1,9 @@
+import jsonfield
+
 from django.db import models
-from django.db.models import F
 from querystring_parser import parser as queryparser
 
-from survey.models import Country, Survey, Organization, Answer
+from survey.models import Country, Survey, Organization, Answer, Question
 
 
 class Stat(models.Model):
@@ -23,9 +24,41 @@ class SurveyStat(Stat):
 
 class OrganizationStat(Stat):
     organization = models.ForeignKey(Organization, null=True)
-    report_order = models.PositiveIntegerField('Order in reports', default=1, blank=True)
+    ordering = models.PositiveIntegerField('Ordering in reports', default=1, blank=True, db_index=True)
 
 
+class RepresentationTypeMixin(object):
+    TYPE_AVERAGE_PERCENT = 'type_average_percent'
+    TYPE_YES_NO = 'type_yes_no'
+    TYPE_MULTISELECT_TOP = 'type_multiselect_top'
+
+    TYPE_CHOICES = (
+        (TYPE_AVERAGE_PERCENT, 'Average percent representation'),
+        (TYPE_YES_NO, 'Representation for "yes" or "no" answers'),
+        (TYPE_MULTISELECT_TOP, 'Top 1 and top 3 representation for ordered multiselect'),
+
+    )
+    type = models.CharField('Representation Type', choices=TYPE_CHOICES, max_length=50)
+
+
+class Representation(RepresentationTypeMixin, models.Model):
+    survey = models.ForeignKey(Survey)
+    question = models.ForeignKey(Question)
+    ordering = models.PositiveIntegerField('Ordering in reports', default=1, blank=True, db_index=True)
+    label1 = models.CharField('Label 1', max_length=400, default='', blank=True)
+    label2 = models.CharField('Label 2', max_length=400, default='', blank=True)
+    label3 = models.CharField('Label 3', max_length=400, default='', blank=True)
+
+
+class QuestionStat(RepresentationTypeMixin, models.Model):
+    country = models.ForeignKey(Country, blank=True, null=True)
+    survey = models.ForeignKey(Survey, null=True)
+    representation = models.ForeignKey(Representation)
+    data = jsonfield.JSONField()
+    ordering = models.PositiveIntegerField('Ordering in reports', default=1, blank=True, db_index=True)
+
+
+####################### Evaluators ###############################
 class AbstractEvaluator(object):
 
     survey_stat = {}
