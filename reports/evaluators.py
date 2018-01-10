ea3ea09ded184bca12bad70ee8b7fe3e2ed2cdfc
DevCoding226 <ilovedevcoding@gmail.com>
2018-01-10 19:46:45 +0700

    Simple input questions can handle percents

--- reports/evaluators.py
+++ reports/evaluators.py
@@ -46,15 +46,12 @@ class AbstractEvaluator(object):
 
     def type_average_percent_processor(self, question_id, question_data, answer):
         q = self.question_dict[question_id]
-        if q.type != Question.TYPE_TWO_DEPENDEND_FIELDS:
+        if (q.type != Question.TYPE_TWO_DEPENDEND_FIELDS and
+                (q.type != Question.TYPE_SIMPLE_INPUT or q.field != Question.FIELD_PERCENT)):
             raise ValueError("type_average_percent_processor doesn't process %s, Question: %s" % (q.type, q.pk))
 
-        if 'main' in question_data:
-            main_str = question_data['main'].strip()
-        else:
-            main_str = ''
-
-        additional_str = question_data['additional'].strip()
+        main_str = question_data['main'].strip() if 'main' in question_data else ''
+        additional_str = question_data['additional'].strip() if 'additional' in question_data else ''
         if not main_str and not additional_str:
             return
 
