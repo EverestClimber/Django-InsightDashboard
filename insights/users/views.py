98acf9aeb73a0b134318e3384b8fe2e2d87d215a
DevCoding226 <ilovedevcoding@gmail.com>
2018-02-03 03:16:29 +0700

    little fixes

--- insights/users/views.py
+++ insights/users/views.py
@@ -62,7 +62,7 @@ class UserSetPasswordView(LoginRequiredMixin, PermissionRequiredMixin, SuccessMe
         return super(UserSetPasswordView, self).form_valid(form)
 
     def get_success_message(self, cleaned_data):
-        return _('Password for %(username)s is changed successfully.' % {'username': self.user.name})
+        return _('Password for %(username)s is changed successfully.') % {'username': self.user.name}
 
 
 class DeleteUser(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
