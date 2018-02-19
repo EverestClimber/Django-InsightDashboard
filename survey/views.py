d93ad8eb3559e8d45788a63671432b4dc4e77baf
DevCoding226 <ilovedevcoding@gmail.com>
2018-02-19 22:34:23 +0200

    removed unused import.

--- survey/views.py
+++ survey/views.py
@@ -1,4 +1,3 @@
-from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.contrib import messages
