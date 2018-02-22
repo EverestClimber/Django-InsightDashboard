5236dc0f5482efbed00b6122cafb7ed3ad9e4c0d
DevCoding226 <ilovedevcoding@gmail.com>
2018-02-22 19:32:07 +0200

    rolledback changes for clear survey.

--- survey/urls.py
+++ survey/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     url(r'^list/$', views.SurveyListView.as_view(), name='list'),
     url(r'^definition/$', views.definition_view, name='definition'),
     url(r'^start/(?P<survey_id>\d+)/?$', views.start_view, name='start'),
-    url(r'^clear/(?P<survey_id>\d+)/?$', views.delete_survey_data, name='delete_survey_data'),
     url(r'^pass/(?P<id>\d+)$', views.pass_view, name='pass'),
     url(r'^preview/(?P<survey_id>\d+)/$', views.preview_view, name='preview'),
     url(r'^thanks/(?P<survey_id>.+)/?$', TemplateView.as_view(template_name='survey/thanks.html'), name='thanks'),
